72ee2022209a69c9c8b9f0a3c8abf852fff3d98b
QsanJohnson <johnson.cheng@qsan.com>
2022-07-27 05:13:56 -0400

    Support ResizeVolume function.

--- volume_test.go
+++ volume_test.go
@@ -27,6 +27,8 @@ func TestVolume(t *testing.T) {
 	createDeleteVolumeTest(t, 5120, &options1)
 	createDeleteVolumeTest(t, 10240, &options2)
 
+	resizeVolumeTest(t)
+
 	exportUnexportVolumeTest(t)
 }
 
@@ -59,6 +61,64 @@ func createDeleteVolumeTest(t *testing.T, volSize uint64, options *VolumeCreateO
 	fmt.Printf("  A volume was deleted. Id:%s\n", vol.ID)
 
 	fmt.Println("createDeleteVolumeTest Leave")
+}
+
+func resizeVolumeTest(t *testing.T) {
+	fmt.Println("resizeVolumeTest Enter")
+	now := time.Now()
+	timeStamp := now.Format("20060102150405")
+	volName := "gotest-vol-" + timeStamp
+	scId := testConf.scId
+	var volSize uint64 = 1024
+
+	options := VolumeCreateOptions{}
+	vol, err := testConf.volumeOp.CreateVolume(ctx, scId, volName, volSize, &options)
+	if err != nil {
+		t.Fatalf("createVolume failed: %v", err)
+	}
+	fmt.Printf("  A volume was created. Id:%s, path: %s \n", vol.ID, vol.VMPath)
+
+	volSize = 5120
+	err = testConf.volumeOp.ResizeVolume(ctx, scId, vol.ID, volSize)
+	if err != nil {
+		t.Fatalf("resizeVolumeTest failed: %v", err)
+	}
+	vols, err := testConf.volumeOp.ListVolumes(ctx, scId, vol.ID)
+	if err != nil {
+		t.Fatalf("ListVolumes failed: %v", err)
+	}
+	if len(*vols) != 1 {
+		t.Fatalf("Volume %s not found.", vol.ID)
+	}
+	if (*vols)[0].SizeMB != volSize {
+		t.Fatalf("resizeVolumeTest failed: size is not match (%d vs %d)", (*vols)[0].SizeMB, volSize)
+	}
+	fmt.Printf("  A volume with ID %s was resize to %d MB\n", vol.ID, volSize)
+
+	volSize = 10240
+	err = testConf.volumeOp.ResizeVolume(ctx, scId, vol.ID, volSize)
+	if err != nil {
+		t.Fatalf("resizeVolumeTest failed: %v", err)
+	}
+	vols, err = testConf.volumeOp.ListVolumes(ctx, scId, vol.ID)
+	if err != nil {
+		t.Fatalf("ListVolumes failed: %v", err)
+	}
+	if len(*vols) != 1 {
+		t.Fatalf("Volume %s not found.", vol.ID)
+	}
+	if (*vols)[0].SizeMB != volSize {
+		t.Fatalf("resizeVolumeTest failed: size is not match (%d vs %d)", (*vols)[0].SizeMB, volSize)
+	}
+	fmt.Printf("  A volume with ID %s was resize to %d MB\n", vol.ID, volSize)
+
+	err = testConf.volumeOp.DeleteVolume(ctx, scId, vol.ID)
+	if err != nil {
+		t.Fatalf("DeleteVolume failed: %v", err)
+	}
+	fmt.Printf("  A volume was deleted. Id:%s\n", vol.ID)
+
+	fmt.Println("resizeVolumeTest Leave")
 }
 
 func exportUnexportVolumeTest(t *testing.T) {
